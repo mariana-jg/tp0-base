7c1ea2ee94ecc4c73bd1fc563f2f284980904b6a
mariana-jg <mjuarezg@fi.uba.ar>
2025-08-22 19:22:46 -0300

    feat: agrego solución inicial al ejercicio

---
+++ mi-generador.py
@@ -0,0 +1,61 @@
+import sys
+
+# Constantes con el contenido del archivo .yaml original
+HEADER = """
+name: tp0
+services:
+"""
+
+BLOQUE_SERVIDOR = """
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+
+BLOQUE_REDES = """
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+# Cliente parametrizable utilizando format
+def bloque_cliente(n):
+    return f"""
+  client{n}:
+    container_name: client{n}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={n}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+
+if __name__ == "__main__":
+    argumentos = sys.argv
+    if len(argumentos) > 2:
+        salida = argumentos[1]
+        cantidad_de_clientes = int(argumentos[2])
+        # with open para asegurar el cierre del archivo
+        with open(salida, 'w') as archivo:
+            archivo.write(HEADER)
+            archivo.write(BLOQUE_SERVIDOR)
+            for n in range(1, cantidad_de_clientes + 1):
+                archivo.write(bloque_cliente(n))
+            archivo.write(BLOQUE_REDES)    
+    else:
+        print("Cantidad de argumentos insuficiente")    
+
+        
