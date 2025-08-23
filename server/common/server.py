ed000f147005ab6e5610e1946701161c36a33866
mariana-jg <mjuarezg@fi.uba.ar>
2025-08-23 13:00:28 -0300

    feat: graceful shutdown en el server

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import signal
 
 
 class Server:
@@ -8,6 +9,13 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._running = True
+        signal.signal(signal.SIGTERM, self.shutodwn)
+
+    def shutdown(self, signum, frame):
+        self._running = False
+        self._server_socket.close()
+        logging.info(f'action: graceful_shutdown | result: sucess')
 
     def run(self):
         """
@@ -20,9 +28,12 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while self._running:
+            try:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+            except OSError as error:
+                break                
 
     def __handle_client_connection(self, client_sock):
         """
