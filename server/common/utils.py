3fa094506fec1020108daefe702ad4130a22cd32
mariana-jg <mjuarezg@fi.uba.ar>
2025-08-24 17:13:56 -0300

    update: separacion de responsabilidades

--- server/common/utils.py
+++ server/common/utils.py
@@ -48,27 +48,4 @@ def load_bets() -> list[Bet]:
         reader = csv.reader(file, quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             yield Bet(row[0], row[1], row[2], row[3], row[4], row[5])
-
-
-"""
-Made to avoid short writes.
-"""
-def avoid_short_writes(socket, data):
-    total = len(data)
-    sent = 0
-    while sent < total:
-        pending = data[sent:]
-        sent += socket.send(pending)
-
-"""
-Made to avoid short reads.
-"""
-def avoid_short_reads(socket, expected_length):
-    data = bytearray()
-    while len(data) < expected_length:
-        packet = socket.recv(expected_length-len(data))
-        if len(packet) == 0:
-            return None
-        else:
-            data.extend(packet)
-    return data        
+      
