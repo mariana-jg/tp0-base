effa1e82b02ba12e834bae9bbff43b553aebc881
mariana-jg <mjuarezg@fi.uba.ar>
2025-09-02 18:41:44 -0300

    update: no serializo con srtruct

--- server/common/server.py
+++ server/common/server.py
@@ -64,7 +64,7 @@ class Server:
             logging.info(f'action: receive_message | result: success | ip: {addr[0]}')
             store_bets([bet])
             logging.info(f'action: apuesta_almacenada | result: success | dni: {bet.document} | numero: {bet.number}')
-            mustWriteAll(client_sock, struct.pack('>B', 1))
+            mustWriteAll(client_sock, (1).to_bytes(1, "big"))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
