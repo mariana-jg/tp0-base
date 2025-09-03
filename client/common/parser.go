ba4eaddca7288ef5f7901bf68e03178c010da741
mariana-jg <mjuarezg@fi.uba.ar>
2025-09-03 19:24:52 -0300

    misc: acomodo indentacion

--- client/common/parser.go
+++ client/common/parser.go
@@ -8,17 +8,21 @@ import (
 )
 
 func parseBetLine(line []string, agency int) (*protocol.Bet, error) {
+
 	if len(line) != 5 {
 		return nil, fmt.Errorf("incomplete data")
 	}
+
 	document, err := strconv.ParseUint(line[2], 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("parse document: %w", err)
 	}
+
 	number, err := strconv.ParseUint(line[4], 10, 16)
 	if err != nil {
 		return nil, fmt.Errorf("parse number: %w", err)
 	}
+
 	return protocol.NewBet(
 		uint8(agency),
 		line[0],
